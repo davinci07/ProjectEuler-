f00a0206f69e1a0cf4a282ef66438198038f783f
Nikita Lysov <lysov.nikita@gmail.com>
2017-04-02 01:56:57 -0500

    Update euler_1.py

--- euler_1.py
+++ euler_1.py
@@ -1,6 +1,2 @@
-multiples = []
-
-for x in range(1,1000):
-    if (x%3 == 0) or (x%5 ==0):
-        multiples.append(x)
+multiples = [long(x) for x in range(1,n) if((x % 3.0 == 0) or (x % 5.0 == 0))]
 print (sum(multiples))
