b9158b49d6c23e97def417b338b949e32f2209e9
Nikita Lysov <lysov.nikita@gmail.com>
2018-08-24 13:12:36 -0500

    Update euler_1.py

--- euler_1.py
+++ euler_1.py
@@ -1,1 +1,14 @@
+#1 
 print(sum([i for i in range(1000) if (i % 3 == 0 or i % 5 == 0)]))
+
+#2 
+a, b, fib = 0, 1, []
+while b < ((4 * (10 ** 6))):
+    a += b
+    b += a
+    fib.extend([a, b])
+fib = sum(list(filter(lambda y: y < (4 * (10 ** 6)), filter(lambda x: x % 2 == 0, fib))))
+print(fib)
+
+#3
+    
