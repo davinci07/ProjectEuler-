fb3683718d6ce7aadf249ea827db300d32fa1c9a
Nikita Lysov <lysov.nikita@gmail.com>
2018-08-24 12:28:40 -0500

    Update euler_2.py

--- euler_2.py
+++ euler_2.py
@@ -1,12 +1,1 @@
-def fib(n):
-    fib1 = [0,1]
-    evenfib = []
-    for x in range(1,n):
-        fib1.append(fib1[x] + fib1[x - 1])
-    for z in range(n):
-        if fib1[z] >= (4 * (10 ** 6)):
-            break
-        if fib1[z] % 2 == 0:
-            evenfib.append(fib1[z])
-    print sum(evenfib)
-fib(100)
+
